da507b97532b5299e3f516551d237b46eb0cdd08
LexSava <lexsava@gmail.com>
2020-11-17 22:17:02 +0300

    feat: changed the hide items function
    Changed the hide items function.

--- DOM-company/dist/main.1f19ae8e.js
+++ DOM-company/dist/main.1f19ae8e.js
@@ -228,7 +228,14 @@ function makeDOMTree(collection, parentDOMEL) {
     for (var i = 0; i < collection.length; i++) {
       var treeItem = collection[i];
       var liEl = document.createElement('li');
-      liEl.innerText = treeItem.name;
+      var spanEl = document.createElement('span');
+      spanEl.innerText = treeItem.name; //Проверка есть ли у элемента дети, если есть добавляем '+'
+
+      if (treeItem.children) {
+        liEl.appendChild(getBulltEl());
+      }
+
+      liEl.appendChild(spanEl);
       parentDOMEL.appendChild(liEl);
 
       if (treeItem.children && treeItem.children.length) {
@@ -243,12 +250,30 @@ function makeDOMTree(collection, parentDOMEL) {
 
 
 document.getElementById('dom_tree').addEventListener('click', function (event) {
-  if (event.target.tagName === 'LI') {
-    event.target.children[0].classList.toggle('hidden');
-    console.log(event.target.innerText);
+  if (event.target.tagName === 'I') {
+    event.target.classList.toggle('collapsed');
+    var children = event.target.parentElement.children;
+
+    if (children && children.length) {
+      for (var i = 0; i < children.length; i++) {
+        if (children[i].tagName === 'UL') {
+          children[i].classList.toggle('hidden');
+        }
+      }
+    }
   }
 }); //--------------------------------------------------------
-//Создаем копию исходных данных (массива)
+
+function getBulltEl() {
+  var bulletEl = document.createElement('i'); //icon
+  //     bulletEl.innerHTML = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right-short" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  //     <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+  //   </svg>`;
+
+  bulletEl.classList.add('collapsed');
+  return bulletEl;
+} //Создаем копию исходных данных (массива)
+
 
 function makeArrayCopy(arr) {
   return arr.map(function (item) {
